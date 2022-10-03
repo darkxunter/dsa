75cd5142672d6f9abeaed5dc58b9d6d96296b0f3
Sergey Fokin <tyraelqp@gmail.com>
2022-10-03 16:20:25 +0300

    Added config.php and used in dsa.php

--- dsa.php
+++ dsa.php
@@ -1,3 +1,5 @@
 <?php
 
-for ($i = 0; $i < random_int(1, 100); $i++) echo "DSA!\n";
+$config = require __DIR__ . '/config.php';
+
+for ($i = 0; $i < random_int($config['min'], $config['max']); $i++) echo "DSA!\n";
